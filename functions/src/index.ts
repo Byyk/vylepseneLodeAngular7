8f3ad4d161ad854b58d33be8ff5e33f54411fda4
Byyk <38165149+Byyk@users.noreply.github.com>
2018-11-25 23:39:35 +0100

    functions_remove_users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,14 +2,9 @@ import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 
 import * as authtriggers from './auth';
-
-import { Users } from "./users";
 import { Matches } from './matches';
 
 admin.initializeApp();
-
-// users API endpoint
-export const users = functions.https.onRequest(Users());
 
 // matches API endpoint
 export const matches = functions.https.onRequest(Matches());
