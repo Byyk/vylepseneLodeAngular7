5b50cba062b557b9d83ad0c1f1948827dc6399d1
Byyk <38165149+Byyk@users.noreply.github.com>
2018-11-25 23:38:19 +0100

    functions_code_optimalization

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -2,17 +2,19 @@ import * as functions from "firebase-functions";
 import {Hrac} from "../../../Vylepsenelode/src/app/model/hrac.model";
 import * as admin from "firebase-admin";
 
-export const userRegistred = functions.auth.user().onCreate((user, context) => {
-    // vytvoreni dat
-    const hrac : Hrac = {
-        uid: user.uid,
-        nickName: 'honzík :)',
-        lastMatch: null
-    };
-    return admin.firestore().collection('Users').doc(user.uid).set(hrac);
+export const userRegistred = functions.auth.user()
+    .onCreate((user, context) => {
+        // vytvoreni dat
+        const hrac : Hrac = {
+            uid: user.uid,
+            nickName: 'honzík :)',
+            lastMatch: null
+        };
+        return admin.firestore().collection('Users').doc(user.uid).set(hrac);
 });
 
-export const userDeleted = functions.auth.user().onDelete((user, context) => {
-    // smazani uzivatelských dat z firestore
-    return admin.firestore().collection('Users').doc(user.uid).delete();
+export const userDeleted = functions.auth.user()
+    .onDelete((user, context) => {
+        // smazani uzivatelských dat z firestore
+        return admin.firestore().collection('Users').doc(user.uid).delete();
 });
