3db3a60e6ca30dd700d89683a1fe2fc4c203eaf4
Byyk <38165149+Byyk@users.noreply.github.com>
2018-11-26 22:20:48 +0100

    lobbyupdate

--- src/app/match-making/lobby/lobby.component.ts
+++ src/app/match-making/lobby/lobby.component.ts
@@ -3,6 +3,9 @@ import {Observable} from "rxjs";
 import {Hrac} from "../../model/hrac.model";
 import {Breakpointy} from "../../model/breakpoints.model";
 import {BreakpointObserver} from "@angular/cdk/layout";
+import {LoginService} from '../../services/login.service';
+import {Match} from '../../model/match.model';
+import {MatchMakingService} from '../../services/match-making.service';
 
 @Component({
   selector: 'app-lobby',
@@ -11,11 +14,33 @@ import {BreakpointObserver} from "@angular/cdk/layout";
 })
 export class LobbyComponent extends Breakpointy implements OnInit {
 
+    match$: Observable<Match>;
     hrac$: Observable<Hrac>;
     souper$: Observable<Hrac>;
 
-    constructor(public bpo: BreakpointObserver) {
+    oponentsPhotoUrl$: Observable<string>;
+
+    constructor(
+        public bpo: BreakpointObserver,
+        public ls: LoginService,
+        public mms: MatchMakingService
+    ) {
         super(bpo);
+        this.match$ = this.mms.getMyMatch();
+        this.hrac$ = this.ls.userDataObservable;
+        this.match$.subscribe((data) => {
+            if(data.oponentUid !== "")
+            {
+                this.souper$ = this.ls.afs.doc(`Users/${data.oponentUid}`).valueChanges();
+                console.log(this.ls.userData.uid);
+                console.log(data.creatorUid);
+                console.log(data.creatorUid === this.ls.userData.uid);
+                if(data.creatorUid === this.ls.userData.uid)
+                    this.oponentsPhotoUrl$ = this.mms.qetProfileImageUrlByUid(data.oponentUid);
+                else if(data.oponentUid === this.ls.userData.uid)
+                    this.oponentsPhotoUrl$ = this.mms.qetProfileImageUrlByUid(data.creatorUid);
+            }
+        });
     }
 
     ngOnInit() {
