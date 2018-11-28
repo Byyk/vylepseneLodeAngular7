bc494ce303cba364de0946e16c984b575b97a2c1
Byyk <38165149+Byyk@users.noreply.github.com>
2018-11-28 22:59:10 +0100

    confirm-reset-password_add_logic

--- src/app/confirm-reset/confirm-reset.component.ts
+++ src/app/confirm-reset/confirm-reset.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ResetPasswordFormGroup} from '../login/formGroups';
+import {LoginService} from '../services/login.service';
 
 @Component({
   selector: 'app-confirm-reset',
@@ -7,17 +10,33 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./confirm-reset.component.scss']
 })
 export class ConfirmResetComponent implements OnInit {
-  public oobCode: string;
+  public RPGroup: FormGroup;
 
 
   constructor(
-      public route: ActivatedRoute
-  ) { }
+      public route: ActivatedRoute,
+      private fb: FormBuilder,
+      private ls: LoginService
+  ) {
+    this.RPGroup = ResetPasswordFormGroup(fb);
+  }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.oobCode = params['oobCode'];
+      this.RPGroup.get('code').setValue(params['oobCode']);
     });
   }
 
+  confirmReset() {
+    this.ls.confirmReset(
+      this.RPGroup.get('code').value,
+      this.RPGroup.get('password').value
+    );
+  }
+
+  reset_codeRequired = () : boolean => this.RPGroup.get('code').hasError('required');
+  reset_passwordRequired = () : boolean => this.RPGroup.get('password').hasError('required');
+  reset_passwordLength = () : boolean => this.RPGroup.get('password').hasError('minlength') && !this.RPGroup.get('password').hasError('required');
+  reset_rePasswordRequired = () : boolean => this.RPGroup.get('rePassword').hasError('required');
+  reset_rePasswordMatch = () : boolean => this.RPGroup.get('rePassword').hasError('passwordNoMatch') && !this.RPGroup.get('rePassword').hasError('required');
 }
