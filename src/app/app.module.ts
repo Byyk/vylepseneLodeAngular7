da162ca5830ece2e814ff14930060ddbb1b11035
Byyk <38165149+Byyk@users.noreply.github.com>
2018-11-29 16:42:18 +0100

    add_messiging_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
 
 import { environment } from '../environments/environment';
 import { LoginComponent } from './login/login.component';
@@ -29,8 +30,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './modules/app-routing.module';
 import { MaterialModule } from './modules/material.module';
-import {MatDialog} from '@angular/material';
-import {AppFontAwesomeModule} from './modules/app-font-awesome.module';
+import { MatDialog } from '@angular/material';
+import { AppFontAwesomeModule } from './modules/app-font-awesome.module';
 import { ConfirmResetComponent } from './confirm-reset/confirm-reset.component';
 import { PasswordDialogComponent } from './password-dialog/password-dialog.component';
 
@@ -63,6 +64,7 @@ import { PasswordDialogComponent } from './password-dialog/password-dialog.compo
         AngularFirestoreModule,
         AngularFireAuthModule,
         AngularFireStorageModule,
+        AngularFireMessagingModule,
         AppFontAwesomeModule
     ],
     providers: [
