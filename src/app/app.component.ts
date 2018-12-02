0c69b50d64a80ec5dd404f33f0e5d3878f81cb6e
Byyk <38165149+Byyk@users.noreply.github.com>
2018-12-02 11:57:48 +0100

    massaging-service-worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MessagingService} from "./services/messaging.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vypseneLodeAngular7';
+  message;
+  constructor(
+      public ms: MessagingService
+  ){
+    ms.getPermission();
+    ms.receiveMessage();
+    this.message = ms.currentMessage;
+  }
 }
