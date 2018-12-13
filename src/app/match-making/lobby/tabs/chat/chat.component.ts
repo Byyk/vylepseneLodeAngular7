48facb279f447c992045e7f1d09984e4e64cbdd7
Byyk <38165149+Byyk@users.noreply.github.com>
2018-12-13 06:20:43 +0100

    chat

--- src/app/match-making/lobby/tabs/chat/chat.component.ts
+++ src/app/match-making/lobby/tabs/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {faComment} from '@fortawesome/free-solid-svg-icons';
 
 export interface Message {
@@ -13,11 +13,15 @@ export interface Message {
 })
 export class ChatComponent implements OnInit {
     faComment = faComment;
-
+    @ViewChild('chat')
+    private scrollContainer: ElementRef;
 
     constructor() { }
 
     ngOnInit() {
+        try {
+            this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+        } catch (e) { console.log(e); }
     }
 
 }
