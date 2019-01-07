b726e040c597924620110af98c31a5119180c981
Byyk <38165149+Byyk@users.noreply.github.com>
2019-01-08 00:22:18 +0100

    oponnent-board-pick-weapon

--- src/app/game/boards/oponents-board/oponents-board.component.ts
+++ src/app/game/boards/oponents-board/oponents-board.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Point, PoleModel, StavPole} from '../../../model/pole.model';
 import {GameService} from '../../../services/game.service';
+import {RaketaModel} from '../../../model/raketa.model';
+import {AbilityData} from '../../../model/my-board.model';
 
 @Component({
   selector: 'app-oponents-board',
@@ -10,6 +12,8 @@ import {GameService} from '../../../services/game.service';
 export class OponentsBoardComponent implements OnInit {
     public poles = new Array<PoleModel>();
 
+    private actualRocket: AbilityData;
+
     @ViewChild('list')
     public list;
 
@@ -18,9 +22,14 @@ export class OponentsBoardComponent implements OnInit {
     ) {
         for(let i = 0; i < 21; i++) {
             for(let j = 0; j < 21; j++){
-                this.poles.push({pozice: {x: j, y: i}, state: StavPole.more});
+                this.poles.push({pozice: {x: j, y: i}, state: StavPole.more, hover: false});
             }
         }
+
+        this.actualRocket = {name: "1x1", icon: "",cost: 0, pattern: [{x: 0, y: 0}]};
+        this.gs.actualWeapon.subscribe(weapon => this.actualRocket = weapon);
+    }
+    ngOnInit() {
     }
     getHeight(){
         return this.list._element.nativeElement.offsetHeight + 'px';
@@ -28,8 +37,15 @@ export class OponentsBoardComponent implements OnInit {
     poleclicked(pole: PoleModel) {
 
     }
-
-    ngOnInit() {
+    hover(pole: PoleModel) {
+        this.poles.forEach(_pole => _pole.hover = false);
+        this.actualRocket.pattern.forEach(point => {
+            const soucetBodu = Point.Sum(point, pole.pozice);
+            if(soucetBodu.x >= 0 && soucetBodu.x <= 20 && soucetBodu.y >= 0 && soucetBodu.y <= 20)
+                this.poles[Point.toNumber(soucetBodu)].hover = true;
+        });
     }
-
+    mouseout() {
+        this.poles.forEach(pole => pole.hover = false);
+    }
 }
