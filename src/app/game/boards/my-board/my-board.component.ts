7fc6059891eaee2a4908cdf8e181a3d308545442
Byyk <38165149+Byyk@users.noreply.github.com>
2019-01-08 00:19:30 +0100

    my-board-update

--- src/app/game/boards/my-board/my-board.component.ts
+++ src/app/game/boards/my-board/my-board.component.ts
@@ -1,8 +1,7 @@
-import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {GameService, Mode} from '../../../services/game.service';
 import {Point, PoleModel, StavPole} from '../../../model/pole.model';
 import {LodModel} from '../../../model/lod.model';
-import {AngularFirestore} from '@angular/fire/firestore';
 import {BehaviorSubject} from 'rxjs';
 
 @Component({
@@ -39,9 +38,16 @@ export class MyBoardComponent implements OnInit {
         this.View();
         return false;
     }
-    hover(point: Point){
+    hover(pole: PoleModel){
         if(this.lod != null)
-            this.lod.pozice = point;
+            this.lod.pozice = pole.pozice;
+        this.poles.forEach(_pole => _pole.hover = false);
+        if(pole.dalsicasti != null){
+            for(const cast of pole.dalsicasti) {
+                this.poles.find(_pole => Point.Equals(_pole.pozice, cast.pozice)).hover = true;
+            }
+        }
+        console.log('1');
         this.View();
     }
     View(){
@@ -51,8 +57,10 @@ export class MyBoardComponent implements OnInit {
         this.poles.forEach(pole => pole.lod = undefined);
         this.polozeneLode.forEach(lod => {
             this.poles[Point.toNumber(lod.pozice)].lod = lod.viewData;
-            lod.castiLode.forEach(cast => {
+            const castiLode = lod.castiLode;
+            castiLode.forEach(cast => {
                 this.poles[Point.toNumber(cast.pozice)].state = StavPole.lod;
+                this.poles[Point.toNumber(cast.pozice)].dalsicasti = castiLode;
             });
         });
     }
@@ -98,7 +106,7 @@ export class MyBoardComponent implements OnInit {
     ngOnInit() {
         for(let i = 0; i < 21; i++) {
             for(let j = 0; j < 21; j++){
-                this.poles.push({pozice: {x: j, y: i}, state: StavPole.more});
+                this.poles.push({pozice: {x: j, y: i}, state: StavPole.more, hover: false});
             }
         }
         this.gs.shipSelected.subscribe(data => {
