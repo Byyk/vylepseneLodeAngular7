042a474640540619c937a5ef9371c32418b5eda3
Byyk <38165149+Byyk@users.noreply.github.com>
2019-01-18 19:03:23 +0100

    random_image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-      private http: HttpClient
-  ) { }
+    constructor(
+        private http: HttpClient,
+        private cdr: ChangeDetectorRef
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const a = floor(1.0);
+    }
+
+    getRandomImage(): string {
+        return `https://picsum.photos/${window.innerWidth}/300?random`;
+    }
 }
